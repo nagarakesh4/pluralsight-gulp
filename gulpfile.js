243118a052d18e8b21894d99f8f7ae4bd95ceb33
Venkata Mani Naga Rakesh Buddhiraju <nagarakesh4@gmail.com>
2017-04-05 07:02:50 -0700

    remove all plugins and use load-plugins module with lazy loading, update verbose argument to showAllFiles

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@
 
 //import gulp
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
-var gulpprint = require('gulp-print');
-var gulpif = require('gulp-if');
 var args = require('yargs').argv;
 
+var $ = require('gulp-load-plugins')({lazy: true});
+
 function log(msg) {
-	util.log(util.colors.red(msg));
+	$.util.log($.util.colors.red(msg));
 }
 
 //create task
@@ -21,9 +18,9 @@ gulp.task('vet', function () {
 			'./src/**/*.js',
 		    './*.js'
 	    ])
-		.pipe(gulpif(args.hello, gulpprint()))//if condition on 'verbose' arguments to print gulppprint
+		.pipe($.if(args.showAllFiles, $.print()))//if condition on 'verbose' arguments to print gulppprint
 		//.pipe(gulpprint()) //prints all the source files
-		.pipe(jscs())
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+		.pipe($.jscs())
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
