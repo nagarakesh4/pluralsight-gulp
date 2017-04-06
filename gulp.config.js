b8c3722c6a854c844f0e22d6918401f918e5a2cf
Venkata Mani Naga Rakesh Buddhiraju <nagarakesh4@gmail.com>
2017-04-06 05:47:08 -0700

    add styles task which will compile LESS to CSS

--- gulp.config.js
+++ gulp.config.js
@@ -1,9 +1,19 @@
 module.exports = function() {
+	var clientBase = './src/client/';
+	
 	var config = {
+		temp: './.temp',
+		
+		/**
+		* File paths
+		*/ 
+		
 		alljs: [
 			'./src/**/*.js',
 		    './*.js'
-		]
+		],
+		
+		styles: clientBase + 'styles/styles.less'
 	};
 	return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,22 @@ function log(msg) {
 gulp.task('vet', function () {
 	log('Analyzing source');
     return gulp
-   		.src(config.alljs)
+   		.src(config.alljs)//EXTENDABILITY
 		.pipe($.if(args.showAllFiles, $.print()))//if condition on 'verbose' arguments to print gulppprint
 		//.pipe(gulpprint()) //prints all the source files
 		.pipe($.jscs())
 		.pipe($.jshint())
 		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
+
+//for css compiling from LESS - less, adding prefixes use autoprefixer
+gulp.task('styles', function () {
+	log('Compiling Stylesheets');
+	return gulp
+		.src(config.styles)
+		.pipe($.less())
+		.pipe($.autoprefixer({browsers: ['last 5 version', '> 1%']})) //get the auto prefixer from website
+		//use browsers that are having >1% in market
+		.pipe(gulp.dest(config.temp));
+});
+
